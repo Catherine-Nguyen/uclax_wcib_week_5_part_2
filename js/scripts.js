e77a269f1cab17dc4d3f09f87119b77889933abd
Catherine Nguyen <catherinenguyen@MacBook-Air.local>
2020-10-31 23:40:01 -0700

    v3

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 //document.querySelector('html').style.backgroundColor = 'red';
 
+//Initial variables and functions:
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
+
+const customWeather = document.getElementById('customWeather');
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
 
+//Initial variables and functions:
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 let insertY = ['the soup kitchen','Disneyland','the White House'];
@@ -16,8 +20,11 @@ let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk',
 
 randomize.addEventListener('click', result);
 
+//Placing the event handler and incomplete function:
 function result() {
   let newStory = storyText;
+
+//Completing the result() function:
 
   let xItem = randomValueFromArray(insertX);
   let yItem = randomValueFromArray(insertY);
@@ -31,6 +38,8 @@ function result() {
   if(customName.value !== '') {
     const name = customName.value;
     newStory = newStory.replace('Bob',name);
+  } else {
+    name = 'Bob';
   }
 
   if(document.getElementById("uk").checked) {
@@ -40,6 +49,26 @@ function result() {
     newStory = newStory.replace('300 pounds',weight);
   }
 
+  if(document.getElementById("Italy").checked) {
+    const weight = Math.round(300*0.0714286) + ' stone';
+    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit',temperature);
+    newStory = newStory.replace('300 pounds',weight);
+  }
+
   story.textContent = newStory;
   story.style.visibility = 'visible';
+}
+
+function before(){
+    document.getElementById('myImage')
+    .src="images/summervibe2.png";
+    document.getElementById('message');
+    
+}
+
+function afterr(){
+    document.getElementById('myImage')
+    .src="images/wintervibe.jpg";
+    document.getElementById('message');
 }
